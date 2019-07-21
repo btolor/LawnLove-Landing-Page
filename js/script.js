9c1eec6cb2ad21d039f8021ede92b43a11cf5776
btolor <brandontolor@yahoo.com>
2019-07-21 04:12:13 -0500

    Finished the regular styling for the page. Changed the necessary html to accommodate the styling process and added comments for the jquery code. Created the html for the page's hidden elements and added some jquery code to display those hidden elements on a click event...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-    $(".care").hide();
-    $(".history").hide();
-    $(".consultation").hide();
+
+    /**
+     * Click function to display the full
+     * form when the html element with an 
+     * id of #form-btn is clicked...*/
+    //Calls the click function on the #form-btn element...  
+    $("#form-btn").click(function () { 
+        //Hides the #form-btn html element
+        $("#form-btn").hide();
+        //Displays the .squeezed html element
+        $(".squeezed").show();
+        //Displays the .basic-serv html element
+        $(".basic-serv").show();
+    });
 });
